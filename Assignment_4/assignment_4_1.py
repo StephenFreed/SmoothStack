886fb4e2750c433b17999650e08144133618c077
Stephen Freed <88246643+StephenFreed@users.noreply.github.com>
2022-02-11 01:15:04 -0500

    Added Assignment 4

---
+++ Assignment_4/assignment_4_1.py
@@ -0,0 +1,172 @@
+# assignment_4_1
+
+# Exercise 8:
+
+# 1
+print("~~~~ 1 ~~~~")
+
+
+def func():
+    print("Hello World")
+
+
+func()
+
+# 2
+print("~~~~ 2 ~~~~")
+
+
+def func1(name):
+    print(f"Hi My name is {name}")
+
+
+func1("Google")
+
+# 3
+print("~~~~ 3 ~~~~")
+
+
+def func3(x, y, z):
+    if z is True:
+        return x
+    else:
+        return y
+
+
+z = False
+print(func3("hello", "goodbye", z))
+
+
+# 4
+print("~~~~ 4 ~~~~")
+
+
+def func4(x, y):
+    return x * y
+
+
+print(func4(5, 4))
+
+# 5
+print("~~~~ 5 ~~~~")
+
+
+def is_even(number):
+    if number % 2 == 0:
+        return True
+    else:
+        return False
+
+
+print(is_even(6))
+
+# 6
+print("~~~~ 6 ~~~~")
+
+
+def is_greater(num_1, num_2):
+    if num_1 > num_2:
+        return True
+    else:
+        return False
+
+
+print(is_greater(8, 5))
+
+# 7
+print("~~~~ 7 ~~~~")
+
+
+def sum_of(*args):
+    sum = 0
+    for num in args:
+        sum = sum + num
+    return sum
+
+
+print(sum_of(5, 10, 5))
+
+# 8
+print("~~~~ 8 ~~~~")
+
+
+def only_even(*args):
+    answer_list = []
+    for num in args:
+        if num % 2 == 0:
+            answer_list.append(num)
+    return answer_list
+
+
+print(only_even(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
+
+# 9
+print("~~~~ 9 ~~~~")
+
+
+def upper_and_lower(my_string):
+    answer_string = ""
+    for i, char in enumerate(my_string):
+        if i % 2 == 0:
+            answer_string += char.upper()
+        else:
+            answer_string += char.lower()
+    return answer_string
+
+
+print(upper_and_lower("Stephen"))
+
+# 10
+print("~~~~ 10 ~~~~")
+
+
+# confused on what the output is supposed to be
+def any_odds(number_1, number_2):
+    if number_1 % 2 != 0 or number_2 % 2 != 0:
+        return "greater"
+    else:
+        return "lesser"
+
+
+print(any_odds(7, 8))
+
+# 11
+print("~~~~ 11 ~~~~")
+
+
+def start_with_same_letter(string_1, string_2):
+    if string_1[0].lower() == string_2[0].lower():
+        return True
+    else:
+        return False
+
+
+print(start_with_same_letter("Stephen", "start"))
+
+# 12
+print("~~~~ 12 ~~~~")
+
+
+def double_otherside_7(value):
+    distance = (7 - value) * 2
+    return distance + 7
+
+
+print(double_otherside_7(10))
+
+
+# 13
+print("~~~~ 13 ~~~~")
+
+
+def first_and_fourth(my_string):
+    answer_string = ""
+    for i, char in enumerate(my_string):
+        if i == 0 or i == 3:
+            answer_string += char.upper()
+        else:
+            answer_string += char
+    return answer_string
+
+
+print(first_and_fourth("stephen"))
---
+++ Assignment_4/assignment_4_2.py
@@ -0,0 +1,31 @@
+import csv
+
+# holds list built below from csv
+book_shop_list = []
+
+with open("/Users/stephenfreed/Projects/SmoothStack/Assignment_4/accounting_list.txt", "r") as f:
+    reader = csv.reader(f)  # gets rows of csv list
+    header = next(reader)  # skip header row
+    # loops to build clean list (strip whitespace)
+    for row in reader:
+        stripped_row = []
+        for elm in row:
+            stripped_row.append(elm.strip())
+        book_shop_list.append(stripped_row)
+
+
+# function to calculate tuple
+def calc_product(row):
+    # finds product from correct index of row
+    product = float(row[3]) * float(row[4])
+    # adds 10 if below 100
+    if product < 100:
+        product += 10
+    # lambda sets floats to 2 decimal places
+    rounded_float = lambda x: "{:.2f}".format(x)
+    return (int(row[0]), rounded_float(product))
+
+
+# map to run function on every row of list
+answer = map(calc_product, book_shop_list)
+print(list(answer))
