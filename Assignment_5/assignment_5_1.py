7f5accb335d805c1a0da5a385e296d602a11a863
Stephen Freed <88246643+StephenFreed@users.noreply.github.com>
2022-02-16 21:16:34 -0500

    updates on assignment 5

--- Assignment_5/assignment_5_1.py
+++ Assignment_5/assignment_5_1.py
@@ -24,21 +24,25 @@ def calculate_bmi(data_list) -> list:
 # result of data parsed from txt file below
 people_data_list = []
 
-with open("/Users/stephenfreed/Projects/SmoothStack/Assignment_5/input_data.txt", "r") as input_file:
-    reader = csv.reader(input_file)  # gets rows of txt file
-    header = next(reader)  # skip header row (Input Data:)
-    # gets number of people in input data / skips row / list -> [0] of list -> integer
-    num_of_people = int(next(reader)[0])
-    # loops to build clean list (strip white space)
-    for row in reader:
-        # converts list str to just str and splits on white space
-        # this creates list of the two inputs to work with
-        split_row_list = str(row[0]).split(" ")
-        # cleans and appends new split list with people data to people_data_list
-        stripped_row = []
-        for elm in split_row_list:
-            stripped_row.append(elm.strip())
-        people_data_list.append(stripped_row)
+try:
+    with open("SmoothStack/Assignment_5/input_data.txt", "r") as input_file:
+        reader = csv.reader(input_file)  # gets rows of txt file
+        header = next(reader)  # skip header row (Input Data:)
+        # gets number of people in input data / skips row / list -> [0] of list -> integer
+        num_of_people = int(next(reader)[0])
+        # loops to build clean list (strip white space)
+        for row in reader:
+            # converts list str to just str and splits on white space
+            # this creates list of the two inputs to work with
+            split_row_list = str(row[0]).split(" ")
+            # cleans and appends new split list with people data to people_data_list
+            stripped_row = []
+            for elm in split_row_list:
+                stripped_row.append(elm.strip())
+            people_data_list.append(stripped_row)
+
+except:  # noqa
+    print("\nThere was a problem reading from input_data.txt.")
 
 # calls function on created people_data_list from txt file
 bmi_results = calculate_bmi(people_data_list)
@@ -48,8 +52,11 @@ for result in bmi_results:
     print(result, end=" ")
 
 # writes results to file with some formatting
-with open("bmi_reults.txt", "w") as answer_file:
-    answer_file.write("These are the BMI results...\n")
-    for row in range(num_of_people):
-        answer_row = f"Person {row+1} BMI Result: {bmi_results[row]}\n"
-        answer_file.write(answer_row)
+try:
+    with open("SmoothStack/Assignment_5/bmi_results.txt", "w") as answer_file:
+        answer_file.write("These are the BMI results...\n")
+        for row in range(num_of_people):  # type: ignore
+            answer_row = f"Person {row+1} BMI Result: {bmi_results[row]}\n"
+            answer_file.write(answer_row)
+except:  # noqa
+    print("\nThere was a problem writing results to bmi_results.txt")
