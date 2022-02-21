b0bc250833fb76a03145ecdbff5b0a13a5bc16cf
Stephen Freed <88246643+StephenFreed@users.noreply.github.com>
2022-02-21 18:20:16 -0500

    cleaning up parse_excel_functions.py

--- Mini_Project_2/parse_excel_functions.py
+++ Mini_Project_2/parse_excel_functions.py
@@ -1,41 +1,26 @@
-import re
 import time
+import re
 import datetime
 import subprocess
 import openpyxl
+import validation_functions as validate
 import logging
 
 
-# custom error to handle if anything fails during parsing of excel file
-# error is called after known handled exceptions so the program does not crash
-# errors are logged and then printed to the user for information on what happened
 class ProgramError(Exception):
+
+    """
+    custom error to handle if anything fails during parsing of excel file
+    error is called after known handled exceptions so the program does not crash
+    errors are logged and then printed to the user for information on what happened
+    """
     pass
 
-
-# validate datetime cell and validate month spelled out
-def validate_datetime(cell_string) -> bool:
-    return True
-    date_format = "%Y-%m-%d %H:%M:%S"
-    try:
-        # checks if cell is not empty and that date is in correct format
-        if cell_obj.value is not None and datetime.datetime.strptime(cell_string, date_format):
-            date_and_row = (cell_string, row_number)
-            parsed_date_list.append(date_and_row)
-    # passes if strptime function fails on invalid date format
-    except:  # noqa
-        pass
-
-
-def validate_month_string(cell_string) -> bool:
-    return True
-
-
+# main function to parse target excel data from workbook
 def parse_excel_data(file_name_to_parse: str):
 
     """
-    Parses data based on file name input. Looks for month and year.
-    Returns data from that month and year in the excel file.
+    Todo: write detailed discription of what this function does
     """
 
     # try block if anything goes wrong with parsing logs and displays to User
@@ -52,47 +37,57 @@ def parse_excel_data(file_name_to_parse: str):
             "/Users/stephenfreed/Projects/SmoothStack/Mini_Project_2/excel_files/{}".format(file_name_to_parse)
             )
 
-        # gets list of sheets in workbook
-        if "VOC Rolling MoM" in wb_obj.sheetnames:
-            # sets sheet of workbook
-            sheet_obj = wb_obj["VOC Rolling MoM"]
-            print("sheet was found")
-            print(sheet_obj['B1'].value)
-            print(sheet_obj['E1'].value)
+        # runs function to validate proper sheet present in workbook / returns tuple
+        sheet_present_tuple = validate.check_for_sheet(wb_obj)
 
-        else:
-            logging.error("'VOC Rolling MoM' Sheet Was Not Found In Workbook")
-            print("\n(ERROR) 'VOC Rolling MoM' Sheet Was Not Found In Workbook")
+        # if return tuple has not validated sheet raises ProgramError
+        if sheet_present_tuple[0] is False:
             raise ProgramError
 
+        # if valid sets sheet index in list and proper sheet object to parse from
+        sheet_list_index = sheet_present_tuple[1]
+        sheet_obj = wb_obj[sheet_present_tuple[0]]
 
-
-        # find column number
-        # loop through top row looking for proper date(2 choices)
-        # look for "Full Name" or parse date 01 index is month 0 index is year
+        # finds column number of proper month and year(if present in cell) to parse from
+        # 2 valid cell scenarios: full month spelled out or datetime
         column_to_parse = None
-        m_column = sheet_obj.max_column  # type: ignore
+        m_column = sheet_obj.max_column
+
+        # loop through top row looking for proper date (2 valid choices)
         for column_number in range(1, m_column + 1):
+
             try:
-                header_cell_obj = sheet_obj.cell(row=1, column=column_number)  # type: ignore
+                # gets header cell info and turns it into a string
+                header_cell_obj = sheet_obj.cell(row=1, column=column_number)
                 cell_string = str(header_cell_obj.value).strip()
-                datetime_result = validate_datetime(cell_string)
-                month_string_result = validate_month_string(cell_string)
+
+                # skips blank cells
+                if header_cell_obj.value is None:
+                    continue
+
+                # calls both types of validation functions to see if either is True
+                datetime_result = validate.validate_datetime(cell_string, month_from_filename, year_from_filename)
+                month_string_result = validate.validate_month_string(cell_string, month_from_filename)
+
+                # if either are true sets column number to parse from and breaks loop
                 if datetime_result is True or month_string_result is True:
                     column_to_parse = column_number
+                    break
 
             except: # noqa
-                logging.error("Error While Building Header,Column List In parse_excel_functions.py")
-                print("\n(ERROR) Error While Building Header,Column List In parse_excel_functions.py")
+                logging.error("While Finding Matching Column Name In parse_excel_functions.py")
+                print("\n(ERROR) While Finding Matching Month and Year(if present) In parse_excel_functions.py")
                 raise ProgramError
-        if column_to_parse == None:
-            pass
-            # todo throw error month not found
 
+        # checks if column_to_parse was assigned
+        if column_to_parse is None:
+            logging.error("No Valid Matching Month and Year(if present) Cell Was Found In parse_excel_functions.py")
+            print("\n(ERROR) No Valid Matching Month and Year(if present) Cell Was Found In parse_excel_functions.py")
+            raise ProgramError
 
         # loop through first row looking for correct rows(3 choices)
         # build tuple (row_name, row_number)
-        row_to_parse = []
+        rows_to_parse_list = []
         m_row = sheet_obj.max_row  # type: ignore // gets max row number
         for row_number in range(1, m_row + 1):
             cell_obj = sheet_obj.cell(row=row_number, column=1)  # type: ignore
@@ -100,46 +95,42 @@ def parse_excel_data(file_name_to_parse: str):
             try:
                 if cell_obj.value is None:
                     continue
-
-                split_string_list = re.split(' ', date)  # split string into list on space
-                if split_string_list[0].title() == "Promoters" or 
-                split_string_list[0].title() == "Passives" or
-            split_string_list[0].title() == "Dectractors":
-                row_to_parse.append((split_string_list[0].title(), row_number))
+                split_string_list = re.split(' ', cell_string)  # split string into list on space
+                if (
+                    split_string_list[0].title() == "Promoters" or
+                    split_string_list[0].title() == "Passives" or
+                    split_string_list[0].title() == "Dectractors"
+                     ):
+                    rows_to_parse_list.append((split_string_list[0].title(), row_number))
 
             # passes if strptime function fails on invalid date format
-            except:  # noqa
+            except Exception as e:  # noqa
+                print("something wrong finding rows")
+                print(e)
                 pass
 
-        if len(row_to_parse) != 3:
+        if len(rows_to_parse_list) != 3:
             # todo error missing row fields
-
-
-
-
+            print("error not enough rows found")
 
         # builds logging output for successful parsing of data
         list_of_worksheets = wb_obj.sheetnames  # get workbook list of worksheets
         successful_log_string = ""
         successful_log_string += "\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
         successful_log_string += "Successfully Parsed Excel File: {}\n".format(file_name_to_parse)
-        successful_log_string += "Data From Worksheet: {}\n".format(list_of_worksheets[0])
+        successful_log_string += "Data From Worksheet: {}\n".format(list_of_worksheets[sheet_list_index])
         successful_log_string += (
                                   "~~~ For the Month of {} in {} ~~~\n"
                                   .format(month_from_filename.capitalize(), year_from_filename)
                                   )
 
         # loops through target row concatenating header and row info
-        for header, column_number in header_and_column_list:  # unpacks tuple
+        for row_name, row_number in rows_to_parse_list:  # unpacks tuple
             try:
-                target_row_column_cell_obj = sheet_obj.cell(row=row_of_info, column=column_number)  # type: ignore
+                target_row_column_cell_obj = sheet_obj.cell(row=row_number, column=column_to_parse)  # type: ignore
                 target_row_column_cell = str(target_row_column_cell_obj.value).strip()
-                if target_row_column_cell[:2] == "0.":  # check if % in cell
-                    target_row_column_cell_float = float(target_row_column_cell)
-                    successful_log_string += "{}: {:.2%}\n".format(header, target_row_column_cell_float)
-                else:
-                    target_row_column_cell_int = int(target_row_column_cell)
-                    successful_log_string += "{}: {}\n".format(header, target_row_column_cell_int)
+                # check what level
+                successful_log_string += "{}: {}: say something".format(row_name, target_row_column_cell)
             except: # noqa
                 logging.error("Error While Building successful_log_string In parse_excel_functions.py")
                 print("\n(ERROR) Error While Building successful_log_string In parse_excel_functions.py")
@@ -151,9 +142,10 @@ def parse_excel_data(file_name_to_parse: str):
         logging.error("A Known Exception Was Handled | Application Could Not Finish")
         print("The Known Exception Above Was Handled ^^^\nApplication Could Not Finish\nPlease Check Log File...")
 
-    except: # noqa
+    except Exception as e: # noqa
         logging.error("The Program Could Not Get Excel Data Successfully")
         print("The Program Could Not Get Excel Data Successfully\nTry Another File\nClosed Application...")
+        print(e)
 
     # displays parsed data to user and logs it if there were no exceptions
     else:
@@ -169,3 +161,5 @@ def parse_excel_data(file_name_to_parse: str):
         print("~~~~~~~~~~~~~~~~~~~~~~\n ~ Opening Log File ~\n~~~~~~~~~~~~~~~~~~~~~~")
         time.sleep(1)
         subprocess.call(["open", "/Users/stephenfreed/Projects/SmoothStack/Mini_Project_1/logging/log_file.txt"])
+
+
