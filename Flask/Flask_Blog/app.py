67e27685bd8d3623fa4c0095750b4e732660a814
Stephen Freed <88246643+StephenFreed@users.noreply.github.com>
2022-02-27 13:06:39 -0500

    work in progress

--- Flask/Flask_Blog/app.py
+++ Flask/Flask_Blog/app.py
@@ -8,8 +8,32 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = "639ded68629a54ac"
 
 # SQLite development database // use PostgreSQL for production
-app.config["SQLAlCHEMY_DATABASE_URI"] = "sqlite:///site.db"
+app.config["SQLAlCHEMY_DATABASE_URI"] = "sqlite:////site.db"
 db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
+
 
 posts = [
     {
@@ -61,4 +85,5 @@ def login():
 
 
 if __name__ == "__main__":
+    db.create_all()
     app.run(debug=True)
