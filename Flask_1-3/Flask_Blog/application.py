1c98e2b666cb0a6ae1aa3c023a539797d2b2235f
Stephen Freed <88246643+StephenFreed@users.noreply.github.com>
2022-03-07 07:44:02 -0500

    comments on application.py

--- Flask_1-3/Flask_Blog/application.py
+++ Flask_1-3/Flask_Blog/application.py
@@ -2,5 +2,5 @@ from app_package import application, db
 
 
 if __name__ == "__main__":
-    db.create_all()
-    application.run(debug=True)
+    db.create_all()  # creates DB if there is not on
+    application.run(debug=True)  # runs application
