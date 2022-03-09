3766a15f30d98675bdd263aa9fd17806df75cfe2
Stephen Freed <88246643+StephenFreed@users.noreply.github.com>
2022-03-09 15:08:22 -0500

    working prototype

--- Flask_1-3/API_Project/app_package/models.py
+++ Flask_1-3/API_Project/app_package/models.py
@@ -1,10 +1,10 @@
 from app_package import db
 
-class VideoModel(db.Model):
+class BlogPostModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    views = db.Column(db.Integer, nullable=False)
+    author = db.Column(db.String(100), nullable=False)
+    title = db.Column(db.String(100), nullable=False)
     likes = db.Column(db.Integer, nullable=False)
 
     def __repr__(self):
-        return f"Video(id = {self.id}, name = {self.name}, views = {self.views}, likes = {self.likes})"
+        return f"Post(id = {self.id}, author = {self.author}, title = {self.title}, likes = {self.likes})"
--- Flask_1-3/API_Project/app_package/resources.py
+++ Flask_1-3/API_Project/app_package/resources.py
@@ -1,81 +1,94 @@
 from flask import jsonify
 from flask_restful import Resource, reqparse, abort, fields, marshal_with
-from app_package import api, db
-from app_package.models import VideoModel
+from app_package import application, api, db
+from app_package.models import BlogPostModel
+
 
 # argument parser
-video_put_args = reqparse.RequestParser()
-video_put_args.add_argument("name", type=str, help="Name Of The Video Missing", required=True)
-video_put_args.add_argument("views", type=int, help="Views Of The Video Missing", required=True  )
-video_put_args.add_argument("likes", type=int, help="Likes Of The Video Missing", required=True)
+blogpost_post_args = reqparse.RequestParser()
+blogpost_post_args.add_argument("author", type=str, help="Name Of Author Missing", required=True)
+blogpost_post_args.add_argument("title", type=str, help="Name Of Title Missing", required=True)
+blogpost_post_args.add_argument("likes", type=int, help="Likes Of The Post Missing", required=True)
 
-
-video_update_args = reqparse.RequestParser()
-video_update_args.add_argument("name", type=str, help="Name Of The Video Missing", required=False)
-video_update_args.add_argument("views", type=int, help="Views Of The Video Missing", required=False)
-video_update_args.add_argument("likes", type=int, help="Likes Of The Video Missing", required=False)
-
+# put to change no fields requred
+blogpost_put_args = reqparse.RequestParser()
+blogpost_put_args.add_argument("author", type=str, help="Name Of Author Missing", required=False)
+blogpost_put_args.add_argument("title", type=int, help="Name of Title Missing", required=False)
+blogpost_put_args.add_argument("likes", type=int, help="Likes Of The Post Missing", required=False)
 
 
 resource_fields = {
     "id": fields.Integer,
-    "name": fields.String,
-    "views": fields.Integer,
+    "author": fields.String,
+    "title": fields.String,
     "likes": fields.Integer
 }
+
 
 class HelloWorld(Resource):
     def get(self, name):
         return {"Hello": name}
 
-class Video(Resource):
-    @marshal_with(resource_fields)
-    def get(self, video_id):
-        result = VideoModel.query.filter_by(id=video_id).first()
+
+class BlogPost(Resource):
+    @marshal_with(resource_fields)  # serializes vs resource_fields dict
+    def get(self, post_id):
+        result = BlogPostModel.query.filter_by(id=post_id).first()
         if not result:
-            abort(404, message="Could Not Find Id...")
+            abort(404, message="Could Not Find Post Id...")
         return result
 
     @marshal_with(resource_fields)
-    def post(self, video_id):
-        args = video_put_args.parse_args()
-        in_db = VideoModel.query.filter_by(id=video_id).first()
+    def post(self, post_id):
+        args = blogpost_post_args.parse_args()
+        in_db = BlogPostModel.query.filter_by(id=post_id).first()
         if in_db:
-            abort(409, message="Video id taken...")
-        video = VideoModel(id=video_id, name=args["name"], views=args["views"], likes=args["likes"],)
-        db.session.add(video)
+            abort(409, message="Post ID Already Exists...")
+        blog_post = BlogPostModel(id=post_id, author=args["author"], title=args["title"], likes=args["likes"])
+        db.session.add(blog_post)
         db.session.commit()
-        return video, 201
+        return blog_post, 201
 
     @marshal_with(resource_fields)
-    def put(self,video_id):
-        args = video_update_args.parse_args()
-        result = VideoModel.query.filter_by(id=video_id).first()
+    def put(self, post_id):
+        args = blogpost_put_args.parse_args()
+        result = BlogPostModel.query.filter_by(id=post_id).first()
         if not result:
-            abort(404, message="Video doesn't exist, Can't Patch")
-        if args["name"]:
-            result.name = args["name"]
-        if args["views"]:
-            result.views = args["views"]
+            abort(404, message="Post Does Not Exist - Can't Update")
+        if args["author"]:
+            result.author = args["author"]
+        if args["title"]:
+            result.title = args["title"]
         if args["likes"]:
             result.likes = args["likes"]
         db.session.commit()
         return result
 
 
-
-    def delete(self, video_id):
-        result = VideoModel.query.filter_by(id=video_id).first()
+    def delete(self, post_id):
+        result = BlogPostModel.query.filter_by(id=post_id).first()
         if not result:
-            return jsonify({"message": "Can't Delete, Id Not Found"})
-        user_obj = db.session.get(VideoModel, video_id)
+            return jsonify({"message": "Can Not Delete, Id Not Found"})
+        user_obj = db.session.get(BlogPostModel, post_id)
         db.session.delete(user_obj)
         db.session.commit()
-        is_deleted = VideoModel.query.filter_by(id=video_id).first()
+        is_deleted = BlogPostModel.query.filter_by(id=post_id).first()
         if is_deleted:
-            return jsonify({"message": "User Was Not deleted"})
-        return jsonify({"message": "User Deleted"})
+            return jsonify({"message": "Post Was Not deleted"})
+        return jsonify({"message": "Post Successfully Deleted"})
+
+
+
+class BlogPostList(Resource):
+    @marshal_with(resource_fields)
+    def get(self):
+        result = BlogPostModel.query.filter_by(id=1).first()
+        if not result:
+            abort(404, message="There Are No Posts...")
+        all_posts = BlogPostModel.query.all()
+        return all_posts
 
 
 api.add_resource(HelloWorld, "/helloworld/<string:name>")
-api.add_resource(Video, "/video/<int:video_id>")
+api.add_resource(BlogPost, "/api/post/<int:post_id>")
+api.add_resource(BlogPostList, "/api/blogpostlist")
